00cbccef8b3934bf2fc5ab17bc357d9341cace44
AndyHegemann <andyhegemann@gmail.com>
2021-11-04 12:27:46 -0600

    Removed Drive Code
    Removed everything not related to the test board

--- FRC-2021/src/main/java/frc/robot/Robot.java
+++ FRC-2021/src/main/java/frc/robot/Robot.java
@@ -7,83 +7,76 @@
 
 package frc.robot;
 
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+ * This is a sample program showing the use of the solenoid classes during operator control. Three
+ * buttons from a joystick will be used to control two solenoids: One button to control the position
+ * of a single solenoid and the other two buttons to control a double solenoid. Single solenoids can
+ * either be on or off, such that the air diverted through them goes through either one channel or
+ * the other. Double solenoids have three states: Off, Forward, and Reverse. Forward and Reverse
+ * divert the air through the two channels and correspond to the on and off of a single solenoid,
+ * but a double solenoid can also be "off", where the solenoid will remain in its default power off
+ * state. Additionally, double solenoids take up two channels on your PCM whereas single solenoids
+ * only take a single channel.
  */
 public class Robot extends TimedRobot {
-	private UsbCamera frontShooterCamera;
-	private UsbCamera rightPanelCamera;
+  private final Joystick m_stick = new Joystick(0);
 
-	private Command m_autonomousCommand;
-	private RobotContainer m_robotContainer;
+  // DoubleSolenoid corresponds to a double solenoid.
+  private final DoubleSolenoid m_solenoidSmall =
+      new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+        // DoubleSolenoid corresponds to a double solenoid.
+
+  private final DoubleSolenoid m_solenoidMedium =
+      new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+        // DoubleSolenoid corresponds to a double solenoid.
+
+  private final DoubleSolenoid m_solenoidLarge =
+      new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
 
 
-	// run when robot is started, put initialization code here
-	@Override
-	public void robotInit() {
-		// create RobotContainer. Does our button bindings & puts autonomous chooser on dashboard
-		m_robotContainer = new RobotContainer();
+  private static final int kSolenoidSmallForward = Constants.GAMEPAD_BUTTON_A;
+  private static final int kSolenoidSmallReverse = Constants.GAMEPAD_BUTTON_B;
+  
+  private static final int kSolenoidMediumForward = Constants.GAMEPAD_BUTTON_X;
+  private static final int kSolenoidMediumReverse = Constants.GAMEPAD_BUTTON_Y;
+  
+  private static final int kSolenoidLargeForward = Constants.GAMEPAD_BUTTON_LEFT_BUMPER;
+  private static final int kSolenoidLargeReverse = Constants.GAMEPAD_BUTTON_RIGHT_BUMPER;
 
-		frontShooterCamera = CameraServer.getInstance().startAutomaticCapture(0);
-		rightPanelCamera = CameraServer.getInstance().startAutomaticCapture(1);
-	}
 
-	// called every robot packet (good for diagnostics), after mode-specific periodics
-	// runs before LiveWindow & SmartDashboard updates
-	@Override
-	public void robotPeriodic() {
-		// runs base periodic functions. Do not delete/comment out
-		CommandScheduler.getInstance().run();
-	}
+  @Override
+  public void teleopPeriodic() {
+    /*
+     * The output of GetRawButton is true/false depending on whether
+     * the button is pressed; Set takes a boolean for whether
+     * to use the default (false) channel or the other (true).
+     *
+     * In order to set the double solenoid, if just one button
+     * is pressed, set the solenoid to correspond to that button.
+     * If both are pressed, set the solenoid will be set to Forwards.
+     */
+    
+    if (m_stick.getRawButton(kSolenoidSmallForward)) {
+      m_solenoidSmall.set(DoubleSolenoid.Value.kForward);
+    } else if (m_stick.getRawButton(kSolenoidSmallReverse)) {
+      m_solenoidSmall.set(DoubleSolenoid.Value.kReverse);
+    }
 
-	@Override
-	public void disabledInit() {}
+    if (m_stick.getRawButton(kSolenoidMediumForward)) {
+      m_solenoidMedium.set(DoubleSolenoid.Value.kForward);
+    } else if (m_stick.getRawButton(kSolenoidMediumReverse)) {
+      m_solenoidMedium.set(DoubleSolenoid.Value.kReverse);
+    }
 
-	@Override
-	public void disabledPeriodic() {}
-
-	// Runs autonomous command selected by {@link RobotContainer} class
-	@Override
-	public void autonomousInit() {
-		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-		// schedule the autonomous command (example)
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.schedule();
-		}
-	}
-
-	@Override
-	public void autonomousPeriodic() {}
-
-	@Override
-	public void teleopInit() {
-		// stops auto before teleop starts running
-		// comment out to continue auto as another command starts
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.cancel();
-		}
-	}
-
-	@Override
-	public void teleopPeriodic() {}
-
-	@Override
-	public void testInit() {
-		// Cancels all running commands at the start of test mode.
-		CommandScheduler.getInstance().cancelAll();
-	}
-
-	@Override
-	public void testPeriodic() {}
+    if (m_stick.getRawButton(kSolenoidLargeForward)) {
+      m_solenoidLarge.set(DoubleSolenoid.Value.kForward);
+    } else if (m_stick.getRawButton(kSolenoidLargeReverse)) {
+      m_solenoidLarge.set(DoubleSolenoid.Value.kReverse);
+    }
+  }
 }
